01c840d8179de9826c1347096fb31380658d5b1d
Mahak85 <150009137+Mahak85@users.noreply.github.com>
2023-11-22 12:44:06 +0530

    Add files via upload

---
+++ BMICalculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your weight in kilograms: ");
+        double weight = scanner.nextDouble();
+
+        System.out.print("Enter your height in meters: ");
+        double height = scanner.nextDouble();
+
+        double bmi = calculateBMI(weight, height);
+
+        System.out.println("Your BMI is: " + bmi);
+
+        displayBMICategory(bmi);
+
+        scanner.close();
+    }
+
+    private static double calculateBMI(double weight, double height) {
+        return weight / (height * height);
+    }
+    
+    private static void displayBMICategory(double bmi) {
+        System.out.print("BMI Category: ");
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("Normal weight");
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
+    }
+}
